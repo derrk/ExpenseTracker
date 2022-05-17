f2b08d7953a5faa3f02074acee76fd17e8cc684b
Derrk <derrik.pollock@gmail.com>
2022-05-16 23:34:08 -0500

    Initial Commit for Expense Tracker App in Swift

--- ExpenseTracker/ContentView.swift
+++ ExpenseTracker/ContentView.swift
@@ -9,13 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+		NavigationView {
+			ScrollView {
+				VStack(alignment: .leading, spacing: 24) {
+					// mark: Title
+					Text("Overview")
+						.font(.title2)
+						.bold()
+					
+				}
+				.padding()
+				frame(maxWidth: .infinity)
+			}
+			.background(Color.background)
+			.navigationBarTitleDisplayMode(.inline)
+			.toolbar {
+				// Mark: Notif icon
+				ToolbarItem {
+					Image(systemName: "bell.badge")
+						.symbolRenderingMode(.palette)
+						.foregroundStyle(Color.icon, .primary)
+				}
+			}
+		}
+		.navigationViewStyle(.stack)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+		Group {
+			ContentView()
+			//ContentView()
+		}
     }
 }
